9fc76deba1e6595bd157bae6431f8fb028bb4a56
Peter Koenders <pkoenders@gmail.com>
2021-12-17 01:04:43 +1300

    aa

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,8 @@ const LayoutWrapper = styled.div`
   width: 100%;
   flex-direction: column;
   min-height: 100vh;
+  padding: 0;
+  margin: 0;
 
   background-color: ${({ theme }) => theme.colors.page.bground.default};
 
