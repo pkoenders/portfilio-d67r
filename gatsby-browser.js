ddca463e9080240f595c17c7e9822d6280ba0af1
Peter Koenders <pkoenders@gmail.com>
2022-07-31 23:43:59 +1200

    update gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -14,14 +14,14 @@ import { PrismicPreviewProvider } from 'gatsby-plugin-prismic-previews'
 // Adds a shared React Context for Prismic preview sessions.
 
 // Provide an update message
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    'This application has been updated. Reload to display the latest version?'
-  )
-  if (answer === true) {
-    window.location.reload()
-  }
-}
+// export const onServiceWorkerUpdateReady = () => {
+//   const answer = window.confirm(
+//     'This application has been updated. Reload to display the latest version?'
+//   )
+//   if (answer === true) {
+//     window.location.reload()
+//   }
+// }
 
 export const wrapPageElement = ({ element }) => (
   // export const wrapRootElement = ({ element }) => (
