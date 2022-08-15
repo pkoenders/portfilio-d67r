66ce1dbb22cf414ae31d23c8aa6f1d95b0801df7
Peter Koenders <pkoenders@gmail.com>
2022-08-16 00:10:03 +1200

    aa

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,12 +26,42 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     'gatsby-plugin-sass',
 
+    // {
+    //   resolve: 'gatsby-plugin-google-tagmanager',
+    //   options: {
+    //     id: 'GTM-M5HMD6B',
+    //     includeInDevelopment: false,
+    //     enableWebVitalsTracking: true,
+    //   },
+    // },
+
     {
-      resolve: 'gatsby-plugin-google-tagmanager',
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        id: 'GTM-M5HMD6B',
-        includeInDevelopment: false,
-        // enableWebVitalsTracking: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'GTM-M5HMD6B', // Google Analytics / GA
+          // 'AW-CONVERSION_ID', // Google Ads / Adwords / AW
+          // 'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: 'OPT_CONTAINER_ID',
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+          // Defaults to https://www.googletagmanager.com
+          origin: 'https://d67r.app',
+        },
       },
     },
 
