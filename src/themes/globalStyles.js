4a03161efe9686cf07c667d742a0a07e7b14860f
Peter Koenders <pkoenders@gmail.com>
2023-03-19 01:39:55 +1300

    !important

--- src/themes/globalStyles.js
+++ src/themes/globalStyles.js
@@ -97,7 +97,7 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.colors.pageHold[100]};
     
     a {
-       color: ${({ theme }) => theme.colors.accent.default};
+       color: ${({ theme }) => theme.colors.accent.default} !important;
     }
 
     form {
